c0f7431499b027f12dbce3c231bce781ac162285
SeongilHeo <tjddlf101@gmail.com>
2021-07-03 04:50:40 +0900

    2nd-허성일

--- 2nd-21.06.29/하-16허성일.py
+++ 2nd-21.06.29/하-16허성일.py
@@ -1,13 +1,18 @@
 def star(i,j,n):
     if ((i//n)%3 == 1) and ((j//n)%3 == 1):
-        print(' ',end='')
+        result[i][j]=' '
     else:
         if n==1:
-            print('*',end='')
+            return
         else:
             star(i,j,n//3)
+            
 n=int(input())
-for i in range(n):
+result=[['*']*n for _ in range(n)]
+for i in range(n//2+1):
     for j in range(n):
-        star(i,j,n)
-    print()
+        star(i,j,n//3)
+
+result[:n//2:-1]=result[:n//2]
+for x in result:
+    print(''.join(x))
