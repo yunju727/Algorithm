2ae1fe9bf56398dcd6a3ac91dadc989d23d8a3b0
haechi <42764085+kimhaech@users.noreply.github.com>
2021-07-22 17:33:24 +0900

    07.22 17:

--- 5st-21.07.20/하-17김태우.py
+++ 5st-21.07.20/하-17김태우.py
@@ -4,10 +4,10 @@ a = 0
 b = N
 while True:
     t = (a+b)//2
-    if N == t**2:
+    if N == t**2: # 제곱근에 해당
         print(t)
         break
-    elif N < t**2:
+    elif N < t**2:  # 제곱근 값이 작은경우
         b = t
-    elif N > t**2:
+    elif N > t**2:  # 제곱근 값이 큰 경우
         a = t
